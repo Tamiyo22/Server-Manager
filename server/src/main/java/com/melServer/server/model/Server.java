cdafed9b4256f7eb5e82526ec77b7f68b8f83031
Melissa Aybar <melissaeaybar@gmail.com>
2023-05-03 17:58:12 -0500

    annotations added for MySql

--- server/src/main/java/com/melServer/server/model/Server.java
+++ server/src/main/java/com/melServer/server/model/Server.java
@@ -1,13 +1,30 @@
 package com.melServer.server.model;
 
 import com.melServer.server.enumeration.Status;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
- * @author
+ * @Melissa H
  * 5/3/2022
  */
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Server {
+    @Id
+    @GeneratedValue(strategy = AUTO)
     private Long id;
+    @Column(unique = true) /* one ip address allowed in database */
+    @NotEmpty(message = "IP Address cannot be empty")
     private String ipAddress;
     private String name;
     private String memory;
