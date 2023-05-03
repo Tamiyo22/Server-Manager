0311584d3a52e6d0b02a934a7e0726bc7acce040
Melissa Aybar <melissaeaybar@gmail.com>
2023-05-03 18:35:15 -0500

    Server Service interface updated

--- server/src/main/java/com/melServer/server/service/ServerService.java
+++ server/src/main/java/com/melServer/server/service/ServerService.java
@@ -1,4 +1,14 @@
 package com.melServer.server.service;
 
+import com.melServer.server.model.Server;
+
+import java.util.Collection;
+
 public interface ServerService {
+    Server create(Server server);
+    Collection<Server> list(int limit);
+    Server get(Long id);
+    Server update(Server server);
+    Boolean delete(Long id);
+
 }
